c03d6c02f99888716b5d36263c98e731fdc0cac2
Faber Andrés Vergara Holguín <fabervergara@gmail.com>
2020-07-01 08:44:02 -0500

    add single request

--- server.js
+++ server.js
@@ -63,10 +63,26 @@ const RootQueryType = new GraphQLObjectType({
   name: 'Query',
   description: 'Root Query',
   fields: () => ({
+    book: {
+      type: BookType,
+      description: 'A Single Book',
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve: (parent, args) => books.find(book => book.id === args.id )
+    },
     books: {
       type: new GraphQLList(BookType),
       description: 'List of All Books',
       resolve: () => books
+    },
+    author: {
+      type: AuthorType,
+      description: 'A Single Author',
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve: (parent, args) => authors.find(author => author.id === args.id)
     },
     authors: {
       type: new GraphQLList(AuthorType),
